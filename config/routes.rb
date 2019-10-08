362ac7e16c5816830099d98fda04c5a55b6b5368
John Kiragu <mutuakiragu@gmail.com>
2019-10-08 20:00:10 +0300

    Add apis and routes for user creation and all

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+
+  scope :api, defaults: { format: :json} do
+    devise_for :users, controllers: { sessions: :sessions},
+               path_names:{ sign_in: login}
+    resource :user, only: [:show, :update]
+  end
+
 end
