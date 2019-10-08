eedfafd50feebfbf98203160fb7d4efd3a4278b6
John Kiragu <mutuakiragu@gmail.com>
2019-10-08 22:50:04 +0300

    Add articles controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,6 @@
 class ArticlesController < ApplicationController
-end
+
+  @articles = Article.all.includes(:user)
+  @articles = Article.authored_by(params[:author]) if params [:author].present?
+  @articles = Article.favorited_by(params[:favorited]) if params[:favorited].present?
+ end
