fa16c2c6a3bccd91a08064637ddb4ee7ca767e8b
John Kiragu <mutuakiragu@gmail.com>
2019-10-08 17:39:54 +0300

    Generate the user controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,14 @@
+class SessionsController < Devise::SessionsController
+
+  def create
+
+    user = User.find_by_email(sign_in_params[:email])
+
+    if user && user.valid_password(sign_in_params[:password])
+      @current_user = user
+    else
+      # Does this even make sense
+      render json: { errors: { 'email or password' => ['is invalid'] } }, status: :unprocessable_entity
+    end
+  end
+end
