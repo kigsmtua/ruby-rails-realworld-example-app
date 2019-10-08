11dcff2fca71fe5e29574f2ff49a6fae21064328
John Kiragu <mutuakiragu@gmail.com>
2019-10-08 13:29:37 +0300

    Remove act as taggable,followable

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,4 +3,12 @@ class Article < ApplicationRecord
   has_many :comment, :dependent, :destroy
   has_many :favorite, :dependent, :destroy
 
+  # This means that the slug is not generated here
+  # What do you need to generate a slug
+  # Taggable , you pass a taglist that is associated by the user
+  # Does this even make any sense ?
+  validates :title, presence:true, allow_blank: true
+  validates :body, presence:true, allo_blank:false
+
+
 end
--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,4 +1,8 @@
 class Favorite < ApplicationRecord
   belongs_to :user
   belongs_to :comment
+
+
+  # Does this mean that such values need to come back for usage?
+  # This means that we can use such values up for manufacturing
 end
