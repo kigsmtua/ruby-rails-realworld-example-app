4ba1f8e9ce337200047d851427e8ee3dd7c101ed
John Kiragu <mutuakiragu@gmail.com>
2019-10-08 23:22:45 +0300

    Add tests for all failure scenarions for the user

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,61 @@
 class ArticlesController < ApplicationController
 
-  @articles = Article.all.includes(:user)
-  @articles = Article.authored_by(params[:author]) if params [:author].present?
-  @articles = Article.favorited_by(params[:favorited]) if params[:favorited].present?
+
+  def index
+    @articles = Article.all.includes(:user)
+    @articles = Article.authored_by(params[:author]) if params [:author].present?
+    @articles = Article.favorited_by(params[:favorited]) if params[:favorited].present?
+
+    @articles_count = @articles.count
+
+    @articles.order(created_at: :desc).offset(params[:offset] || 0).limit(params[:limit] || 20)
+  end
+
+  # This is what happens when you dont include the values that come up for usage hre
+  def feed
+    @articles = Article.includes(:user).where(user: current_user.following_users)
+
+    @articles_count = @articles.count
+
+    @articles = @articles.order(created_at: :desc).offset(params[:offset] || 0).limit(params[:limit] || 20)
+
+    render :index
+  end
+
+
+  # This is what happens when such users come up and use the values that come hete
+  def create
+    @article = Article.new(article_params)
+    @article.user = current_user
+
+    if @article.save
+      render :show
+    else
+      render json: { errors: @article.errors }, status: :unprocessable_entity
+    end
+  end
+
+  # At this point we intend to pass all values that come up along here and join the article params for usage here
+  # Does this mean
+  def show
+    @article = Article.find_by_slug!(params[:slug])
+  end
+
+  def destroy
+    @article = Article.find_by_slug!(params[:slug])
+
+    if @article.user_id == @current_user_id
+      @article.destroy
+
+      render json: {}
+    else
+      render json: { errors: { article: ['not owned by user'] } }, status: :forbidden
+    end
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :body, :description)
+  end
  end
