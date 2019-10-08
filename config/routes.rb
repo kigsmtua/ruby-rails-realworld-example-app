5aa79aaa9cbf4b20e34a6945c767d5166ccef47d
John Kiragu <mutuakiragu@gmail.com>
2019-10-08 20:06:26 +0300

    Add routes for use by the application

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
 
-
+  # This is what happens when such values come along
+  # And store the user and the values come here
+  # You can
   scope :api, defaults: { format: :json} do
     devise_for :users, controllers: { sessions: :sessions},
-               path_names:{ sign_in: login}
+               path_names:{ sign_in: :login}
     resource :user, only: [:show, :update]
   end
 
