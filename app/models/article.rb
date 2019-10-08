26245b8e78bc565f02b336829d73b13a0ae3a66d
John Kiragu <mutuakiragu@gmail.com>
2019-10-08 13:01:24 +0300

    Added some relationships to the database

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,6 @@
 class Article < ApplicationRecord
   belongs_to :user
-  has_many :comment
+  has_many :comment, :dependent, :destroy
+  has_many :favorite, :dependent, :destroy
+
 end
--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,4 +1,7 @@
 class Favorite < ApplicationRecord
   belongs_to :user
   belongs_to :comment
+
+  # This is what happens that come along the user
+  #
 end
