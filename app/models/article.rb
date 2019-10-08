6a96c622b2b88ad2ffc581ab29c0544e8cb016ac
John Kiragu <mutuakiragu@gmail.com>
2019-10-08 13:39:29 +0300

    Make sure an article has slugs and all

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,12 +3,8 @@ class Article < ApplicationRecord
   has_many :comment, :dependent, :destroy
   has_many :favorite, :dependent, :destroy
 
-  # This means that the slug is not generated here
-  # What do you need to generate a slug
-  # Taggable , you pass a taglist that is associated by the user
-  # Does this even make any sense ?
   validates :title, presence:true, allow_blank: true
   validates :body, presence:true, allo_blank:false
-
+  validates :slug, uniqueness: true, exclusion: { in: ['feed'] }
 
 end
--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -2,7 +2,4 @@ class Favorite < ApplicationRecord
   belongs_to :user
   belongs_to :comment
 
-
-  # Does this mean that such values need to come back for usage?
-  # This means that we can use such values up for manufacturing
 end
