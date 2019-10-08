9ab60c0dfece0a93e5e759c5b4a688b8605002f7
John Kiragu <mutuakiragu@gmail.com>
2019-10-08 13:42:02 +0300

    Add additional fields to the slug column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_08_054808) do
+ActiveRecord::Schema.define(version: 2019_10_08_103846) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2019_10_08_054808) do
     t.integer "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "slug"
     t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
@@ -28,6 +29,15 @@ ActiveRecord::Schema.define(version: 2019_10_08_054808) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["article_id"], name: "index_comments_on_article_id"
+  end
+
+  create_table "favorites", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "comment_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["comment_id"], name: "index_favorites_on_comment_id"
+    t.index ["user_id"], name: "index_favorites_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -40,4 +50,6 @@ ActiveRecord::Schema.define(version: 2019_10_08_054808) do
 
   add_foreign_key "articles", "users"
   add_foreign_key "comments", "articles"
+  add_foreign_key "favorites", "comments"
+  add_foreign_key "favorites", "users"
 end
