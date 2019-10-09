854681a1e525bc7c5b58b65090dcf94602fa1dc9
John Kiragu <mutuakiragu@gmail.com>
2019-10-09 06:38:46 +0300

    Add test database and format the fixture values

--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -1,7 +1,9 @@
 require 'test_helper'
 
+# Seems like active record tests the feature support for all frameworks and \
+# Uses them to aactually generate works for the applicationn
 class ArticleTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "the truth" do
+    assert true
+  end
 end
